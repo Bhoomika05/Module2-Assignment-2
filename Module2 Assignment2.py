731f84923d19eb61310660d2e8d728b67492d4c9
Bhoomika05 <67085102+Bhoomika05@users.noreply.github.com>
2020-06-23 12:35:42 +0530

    Add files via upload

---
+++ Module2 Assignment2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+def maxnum():
+   a=int(input("enter number1  "))
+   b=int(input("enter number2  "))
+   c=int(input("enter number3  "))
+   print("maximum number",max(a,b,c))
+maxnum()
+     
+
+
+# In[80]:
+
+
+def reverse(x):
+  return x[::-1]
+
+t = reverse("pyhton")
+
+print(t)
+
+
+# In[47]:
+
+
+def prime():
+    a=int(input("enter number"))
+    count=0
+    for i in range(2,a):
+        if a%i==0:
+            count=count+1
+        
+    if count==0:
+        print("prime number")
+    else:
+        print("composite number")
+prime()
+
+
+# In[70]:
+
+
+def square():
+    sum=0
+    a=int(input("enter a positive number"))
+    for i in range(1,a+1):
+        sum=sum+(i*i)
+    print("sum of squares",sum)
+square()
+     
+
